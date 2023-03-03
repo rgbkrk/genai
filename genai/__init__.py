49962aeb5f8eac4c6371b2d8d70b184fa10bf0d3
Carol Willing <carolcode@willingconsulting.com>
2023-03-02 18:04:39 -0800

    make more pythonic

--- genai/__init__.py
+++ genai/__init__.py
@@ -1,15 +1,16 @@
+"""Genai IPython magic extensions.
+
+## Magic methods
+
+    - `%%assist`: Assist you in writing code.
+
+## Usage
+
+  `%load_ext genai` to load the extension in other notebook
+  projects
+"""
+
 __version__ = "0.8.0"
-
-"""
-Genai IPython magic extensions.
-
-Magic methods:
-
-%%assist 
-
-Usage:
-  %load_ext genai
-"""
 
 from .assist import assist
 
