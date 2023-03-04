e45708d8f2dcd1fb5a7ec18fdc9ca5f07e2728c1
Kyle Kelley <rgbkrk@gmail.com>
2023-03-03 20:27:33 -0800

    migrate assist to ChatCompletion API
    * Break %%assist up into context gathering, generation, and magic
    * Use the new Chat Completion API from OpenAI

---
+++ genai/context.py
@@ -0,0 +1,57 @@
+"""
+Creates user and system messages as context for ChatGPT, using the history of the current IPython session.
+"""
+
+
+def craft_user_message(code):
+    return {
+        "content": code,
+        "role": "user",
+    }
+
+
+def craft_output_message(output):
+    return {
+        "content": repr(output),
+        "role": "system",
+    }
+
+
+# tokens to idenfify which cells to ignore based on the start
+ignore_tokens = [
+    "# genai:ignore",
+    "#ignore",
+    "# ignore",
+    "%%assist",
+    "get_ipython",
+    "%load_ext",
+    "import genai",
+    "%pip install",
+    "#%%assist",
+]
+
+
+def get_historical_context(ipython, num_messages=5):
+    """Create a series of messages to use as context for ChatGPT."""
+    raw_inputs = ipython.history_manager.input_hist_raw
+
+    # Now filter out any inputs that start with our filters
+    # This has to keep the input index as the key for the output
+    inputs = {}
+    for i, input in enumerate(raw_inputs):
+        if not any(input.startswith(token) for token in ignore_tokens):
+            inputs[i] = input
+
+    outputs = ipython.history_manager.output_hist
+
+    indices = sorted(inputs.keys())
+    context = []
+
+    # We will use the last `num_messages` inputs and outputs to establish context
+    for index in indices[-num_messages:]:
+        context.append(craft_user_message(inputs[index]))
+
+        if index in outputs:
+            context.append(craft_output_message(outputs[index]))
+
+    return context
---
+++ genai/magics.py
@@ -0,0 +1,78 @@
+"""Magic to generate code cells for notebooks using OpenAI's API."""
+
+from IPython import get_ipython
+from IPython.core.magic import register_cell_magic
+from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
+from IPython.display import display
+
+from genai.components import (
+    collapsible_log,
+    completion_made,
+    starting_message,
+    styled_code,
+)
+from genai.context import get_historical_context
+from genai.generate import generate_next_cell
+
+from vdom import div, h3
+
+
+@magic_arguments()
+@argument("--fresh", action="store_true")
+@argument(
+    "--verbose",
+    action="store_true",
+    help="Show a full log in the cell output",
+)
+@argument(
+    "--in-place",
+    action="store_true",
+    help="Replace the current cell with the generated code",
+)
+@register_cell_magic
+def assist(line, cell):
+    progress = display(starting_message(), display_id=True)
+
+    args = parse_argstring(assist, line)
+
+    ip = get_ipython()
+    cell_text = cell.strip()
+
+    context = []
+    if not args.fresh:
+        context = get_historical_context(ip)
+
+    if args.verbose:
+        handle = display(
+            collapsible_log(),
+            display_id=True,
+        )
+        logs = []
+
+        def log(element):
+            # Add VDOM element
+            logs.append(element)
+            handle.update(collapsible_log(logs))
+
+        log(
+            div(
+                h3("magic arguments"),
+                styled_code(line),
+                h3("submission"),
+                styled_code(cell),
+            )
+        )
+
+    generated_text = generate_next_cell(context, cell_text)
+
+    progress.update(completion_made())
+
+    new_cell = f"""# generated with %%assist\n{cell_text}\n{generated_text}"""
+
+    if args.in_place:
+        new_cell = f"""#%%assist {line}\n{cell_text}\n{generated_text}"""
+
+    ip.set_next_input(
+        new_cell,
+        replace=args.in_place,
+    )
