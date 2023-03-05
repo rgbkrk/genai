54b2b2279bae1377f3ed625591965530b3248142
Kyle Kelley <rgbkrk@gmail.com>
2023-03-04 18:01:25 -0800

    document the assist magic

--- genai/magics.py
+++ genai/magics.py
@@ -15,7 +15,7 @@ from genai.generate import generate_next_cell
 @argument(
     "--verbose",
     action="store_true",
-    help="Show a full log in the cell output",
+    help="Show additional information in the cell output",
 )
 @argument(
     "--in-place",
@@ -24,6 +24,50 @@ from genai.generate import generate_next_cell
 )
 @register_cell_magic
 def assist(line, cell):
+    """Generate code cells for notebooks using OpenAI's API.
+
+    Usage:
+
+    ```python
+    %%assist
+    # create a scatterplot from df
+    ```
+
+    Will create a code cell below the current one looking something like this
+
+    ```python
+    # create a scatterplot from df
+
+    df.plot.scatter(x="col1", y="col2")
+    ```
+
+    If you've run previous cells in the notebook, the generated code will be
+    based on the history of the current notebook session.
+
+    That even means that, for example, running `df.columns` in a previous cell
+    will cause generated code to use column names in the future!
+
+    Caveats:
+
+    - Only the last 5 cell executions are provided as context.
+    - The generated code is not guaranteed to be correct, idiomatic, efficient, readable, or useful.
+    - The generated code is not guaranteed to be syntactically correct or even something to write home about.
+
+    There are several options you can use to control the behavior of the magic.
+
+    --fresh: Ignore the history of the current notebook session and generate code from scratch.
+
+    --in-place: Replace the current cell with the generated code.
+
+    --verbose: Show additional information in the cell output.
+
+    Example:
+
+    ```python
+    %%assist --fresh --in-place --verbose
+    # how can I query for pokemon via the PokéAPI?
+    ```
+    """
     progress = display(HTML(starting_message()), display_id=True)
 
     args = parse_argstring(assist, line)
@@ -36,10 +80,9 @@ def assist(line, cell):
         context = get_historical_context(ip)
 
     if args.verbose:
-        display(
-            f"""magic arguments: {line}
-submission: {cell}"""
-        )
+        print("magic arguments:", line)
+        print("submission:", cell)
+        print("context:", context)
 
     generated_text = generate_next_cell(context, cell_text)
 
