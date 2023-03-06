a048cff84a5be5d01a2ef7444595b78fd5a9da94
Kyle Kelley <rgbkrk@gmail.com>
2023-03-05 16:06:56 -0800

    test the --fresh arg (#23)

--- tests/test_magics.py
+++ tests/test_magics.py
@@ -78,3 +78,47 @@ def test_assist_magic_with_args(create, ip):
         "#%%assist --in-place --verbose\ncreate a scatterplot from df\njust like code better",
         replace=True,
     )
+
+
+@mock.patch(
+    "openai.ChatCompletion.create",
+    return_value={
+        "choices": [
+            {
+                "message": {
+                    "role": "assistant",
+                    "content": "superplot(df)",
+                },
+            },
+        ],
+    },
+    autospec=True,
+)
+def test_assist_magic_with_fresh_arg(create, ip):
+    ip.set_next_input = mock.MagicMock()
+
+    ip.run_cell_magic(
+        magic_name="assist",
+        line="--fresh",
+        cell="create a scatterplot from df",
+    )
+
+    create.assert_called_once_with(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": generate.NOTEBOOK_CODING_ASSISTANT_TEMPLATE,
+            },
+            # Note that there is zero other context, due to running with --fresh
+            {
+                "role": "user",
+                "content": "create a scatterplot from df",
+            },
+        ],
+    )
+
+    ip.set_next_input.assert_called_once_with(
+        "create a scatterplot from df\nsuperplot(df)",
+        replace=False,
+    )
