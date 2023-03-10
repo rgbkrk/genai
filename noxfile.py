30b13d1f71f38d51c795528fdb2244bc89b5456f
Matthew Seal <mseal007@gmail.com>
2023-03-10 15:40:49 -0800

    First step adding nox

---
+++ noxfile.py
@@ -0,0 +1,55 @@
+import noxfile
+import nox_poetry
+
+LINT_PATHS = ["genai", "noxfile.py"]
+
+noxfile.options.reuse_existing_virtualenv = True
+noxfile.options.sessions = ["lint", "test"]
+
+
+@nox_poetry.session(python=["3.8", "3.9", "3.10"])
+def test(session: nox_poetry.Session):
+    session.run("pytest", "-v", "--cov=genai")
+
+
+@nox_poetry.session(python="3.9")
+def generate_coverage_xml(session: nox_poetry.Session):
+    session.install("coverage[toml]")
+    session.run("coverage", "xml")
+
+
+@nox_poetry.session(python="3.9")
+def lint(session: nox_poetry.Session):
+    session.notify("black_check")
+    session.notify("flake8")
+    session.notify("isort_check")
+
+
+@nox_poetry.session(python="3.9")
+def flake8(session: nox_poetry.Session):
+    session.install("flake8")
+    session.run("flake8", *LINT_PATHS, "--count", "--show-source", "--statistics", "--benchmark")
+
+
+@nox_poetry.session(python="3.9")
+def black_check(session: nox_poetry.Session):
+    session.install("black")
+    session.run("black", "--check", *LINT_PATHS)
+
+
+@nox_poetry.session(python="3.9")
+def isort_check(session: nox_poetry.Session):
+    session.install("isort")
+    session.run("isort", "--diff", "--check", *LINT_PATHS)
+
+
+@nox_poetry.session(python="3.9")
+def blacken(session: nox_poetry.Session):
+    session.install("black")
+    session.run("black", *LINT_PATHS)
+
+
+@nox_poetry.session(python="3.9")
+def isort_apply(session: nox_poetry.Session):
+    session.install("isort")
+    session.run("isort", *LINT_PATHS)
