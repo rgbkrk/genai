f507d638461012b4a33a79da7dfc38d9029e7f85
Matthew Seal <mseal007@gmail.com>
2023-03-10 19:23:36 -0800

    Attempt to fix CI #4

--- noxfile.py
+++ noxfile.py
@@ -9,6 +9,7 @@ nox.options.sessions = ["lint", "test"]
 
 @nox_poetry.session(python=["3.8", "3.9", "3.10"])
 def test(session: nox_poetry.Session):
+    session.run_always("poetry", "install", external=True)
     session.run("pytest", "-v", "--cov=genai")
 
 
