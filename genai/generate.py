aa89dcea0b634440d8d0bf1f96afc6a6708e3ca6
Kyle Kelley <rgbkrk@gmail.com>
2023-03-23 18:42:55 -0700

    create a prompt store for registration

--- genai/generate.py
+++ genai/generate.py
@@ -2,30 +2,7 @@ from typing import Any, Dict, Iterator, List, TypedDict
 
 import openai
 
-NOTEBOOK_CREATE_NEXT_CELL_PROCLAMATION = """
-As a coding assistant, your task is to help users write code in Python within Jupyter Notebooks. Provide comments and code for the user to read and edit, ensuring it can be run successfully. The user will be able to run the code in the cell and see the output.
-
-When the user is interacting with you their message will start with `%%assist`. Otherwise, they are running commands and getting output from the system.
-
-You can use markdown to format your response. For example, to create a code block, use
-
-```python
-# code
-```
-
-""".strip()  # noqa: E501
-
-
-NOTEBOOK_ERROR_DIAGNOSER_PROCLAMATION = """
-As a coding assistant, you'll diagnose errors in Python code written in a Jupyter Notebook. Format your response using markdown. Making sure to include the language around code blocks, like
-
-```python
-# code
-```
-
-Provide concise code examples in your response which will be rendered in Markdown in the notebook. The user will not be able to respond to your response.
-""".strip()  # noqa: E501
-
+from genai.prompts import PromptStore
 
 Completion = TypedDict(
     "Completion",
@@ -69,7 +46,7 @@ def deltas(completion: Iterator[StreamCompletion]) -> Iterator[str]:
             yield delta["content"]
 
 
-def generate_next_cell(
+def generate_next_from_history(
     context: List[Dict[str, str]],
     text: str,
     stream: bool = False,
@@ -80,7 +57,7 @@ def generate_next_cell(
             # Establish the context of the conversation
             {
                 "role": "system",
-                "content": NOTEBOOK_CREATE_NEXT_CELL_PROCLAMATION,
+                "content": PromptStore.assist_prompt,
             },
             # Presumably In, Out
             *context,
@@ -118,7 +95,7 @@ def generate_exception_suggestion(
     messages.append(
         {
             "role": "system",
-            "content": NOTEBOOK_ERROR_DIAGNOSER_PROCLAMATION,
+            "content": PromptStore.exception_prompt,
         },
     )
 
